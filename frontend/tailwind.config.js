c7823183cc08f1dcf6e011b5fc325f67c31b1517
ammyroy119 <ammyroy119@gmail.com>
2023-10-03 16:44:13 +0530

    integrated flowbite

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
@@ -13,6 +14,7 @@ module.exports = {
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
+    require('flowbite/plugin')
   ],
 }
 
